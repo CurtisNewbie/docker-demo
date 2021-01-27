64d71968b63e73f4e6a88cce29759e8a9fd541a7
CurtisNewbie <zhuangyongj@gmail.com>
2021-01-28 01:02:46 +0800

    Demo for docker-compose

--- spring-on-docker/src/main/java/com/curtisnewbie/config/RedissonConfig.java
+++ spring-on-docker/src/main/java/com/curtisnewbie/config/RedissonConfig.java
@@ -15,7 +15,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.useSingleServer().setAddress("redis://redis-demo:6379");
         return Redisson.create(config);
     }
 }
